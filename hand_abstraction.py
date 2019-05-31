e5ebb62c8cb69685af52ef0f9d32a8a1ceae44bd
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2019-05-30 23:08:25 -0500

    Got a working CFR+ blueprint strategy.

--- hand_abstraction.py
+++ hand_abstraction.py
@@ -1,16 +1,21 @@
 import os
+import pdb
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy import stats
-import pdb
 from tqdm import tqdm, trange
 import pickle
-from rhode_island_holdem import RhodeHand, get_deck
+import pprint
+from rhode_island_holdem import *
 
-N_BUCKETS = 20
+N_PREFLOP_BUCKETS = 13
+N_FLOP_BUCKETS = 10
+N_TURN_BUCKETS = 50
 EQUITY_SAVE_NAME = 'rhode_flop_equities.pkl'
+FLOP_SAVE_NAME = 'rhode_flop_abstraction.pkl'
+TURN_SAVE_NAME = 'rhode_turn_abstraction.pkl'
 N_EQUITY_BINS = 50  # How many bins to divide the equity histogram into
-ITERATIONS = 10    # How many iterations of k-means clustering to run
+ITERATIONS = 100    # How many iterations of k-means clustering to run
 
 # TODO: Run k-means clustering on the Earth Mover's Distances.
 
@@ -163,7 +168,9 @@ def cluster_with_means(matrix, means):
         nearest_distance = np.Inf
         for j, mean in enumerate(means):
             # Compute the Earth Movers Distance (aka Wasserstein Distance)
+            # TODO: Get EMD to work by using the k-means++ initialization.
             # distance = stats.wasserstein_distance(matrix[hand, :], mean)
+            # Using L2 distance for now because EMD isn't clustering properly.
             distance = np.linalg.norm(matrix[hand, :] - mean)
             if distance < nearest_distance:
                 nearest_mean = j
@@ -191,7 +198,7 @@ def update_means(matrix, clusters):
     return means
 
 
-def compute_abstractions():
+def flop_abstraction():
     """Finds similar flop hands and groups them together.
 
     Similarity is based on the Earth Movers Distance of the hands' equity
@@ -200,6 +207,10 @@ def compute_abstractions():
     Returns:
         abstraction -
     """
+    if os.path.isfile(FLOP_SAVE_NAME):
+        return pickle.load(open(FLOP_SAVE_NAME, 'rb'))
+
+    print('[INFO] Computing the flop abstraction...')
     hands = get_equity_distributions()
 
     # Create a matrix of points of shape (n_hands, n_bins)
@@ -210,28 +221,152 @@ def compute_abstractions():
         hands_list.append(hand)
 
     # Sample K initial means initialized to be existing points.
-    means = np.zeros((N_BUCKETS, N_EQUITY_BINS))
+    means = np.zeros((N_FLOP_BUCKETS, N_EQUITY_BINS))
     for i in range(means.shape[0]):
-        random_hand = i #np.random.randint(matrix.shape[0])
+        random_hand = np.random.randint(matrix.shape[0])
         means[i, :] = matrix[random_hand, :]
 
     for i in trange(ITERATIONS):
         clusters = cluster_with_means(matrix, means)
         means = update_means(matrix, clusters)
 
-    cluster_idx = {}
+    abstraction = {}
     for idx, cluster in enumerate(clusters):
         for hand in cluster:
             hand_string = hands_list[hand]
-            cluster_idx[hand_string] = idx
+            abstraction[hand_string] = idx + N_PREFLOP_BUCKETS
 
-    return cluster_idx
+    pickle.dump(abstraction, open(FLOP_SAVE_NAME, 'wb'))
+    return abstraction
+
+
+def preflop_abstraction():
+    """Returns the abstraction used for the preflop.
+
+    The only abstraction used here is just grouping all ranks together; that is,
+    it drops the suit information.
+
+    Returns:
+        abstraction - A dictionary mapping preflop hands to their abstact bucket
+            index.
+    """
+    abstraction = {}
+    for card in get_deck():
+        abstraction[card] = Card(card).rank - 2     # Puts 2 in the 0th bucket
+    return abstraction
+
+
+def turn_equity(hole, flop, turn):
+    """Returns the player's chance of winning over all possible opponent hands.
+
+    This assumes an even distribution of opponent hands, and calculates the
+    expected equity.
+
+    Inputs:
+        hole - Player's hole card
+        flop - Flop card
+        turn - Turn card
+
+    Returns:
+        equity - Expected equity of this hand.
+    """
+    equity = 0
+    for opponent_card in get_deck():
+        if opponent_card not in (hole, flop, turn):
+            player_hand = RhodeHand(hole, flop, turn)
+            opponent_hand = RhodeHand(opponent_card, flop, turn)
+            if player_hand > opponent_hand:
+                equity += 1
+            elif player_hand == opponent_hand:
+                equity += 0.5
+    return equity / 49      # There are 49 (52 - 3) possible opponent cards
+
+
+# TODO: Are these abstractions good? Evaluate abstraction quality.
+def turn_abstraction():
+    """Returns the abstraction dictionary used for the turn.
+
+    Here, we only need to cluster the equity (expected hand strength), since
+    there are no equity distributions.
+    """
+    if os.path.isfile(TURN_SAVE_NAME):
+        return pickle.load(open(TURN_SAVE_NAME, 'rb'))
+
+    print('[INFO] Computing turn abstractions...')
+    # This finds the largest gaps between equity values and segments based on
+    # that. TODO: If this is not powerful enough, try Kernel Density Estimation (KDE).
+    equities = []
+    hand_list = []   # To keep track of which hand corresponds to which equity
+    with tqdm(range(52 * 51 * 50)) as t:
+        for hole in get_deck():         # Iterates over all possible hole/flop/turn combos
+            for flop in get_deck():
+                if hole == flop:
+                    continue
+                for turn in get_deck():
+                    if turn in (hole, flop):
+                        continue
+
+                    equity = turn_equity(hole, flop, turn)
+                    equities.append(equity)
+                    hand_list.append(hole + flop + turn)
+                    t.update()
+
+    equities = np.array(equities)
+    hand_list = np.array(hand_list)
+    sort_indices = np.argsort(equities)
+    equities = equities[sort_indices]
+    hand_list = hand_list[sort_indices]
+    differences = np.diff(equities)
+    largest_diff_locations = np.argsort(differences)[-N_TURN_BUCKETS+1:]
+
+    abstraction = {}
+    prev_idx = 0
+    for label, d in enumerate(sorted(largest_diff_locations)):
+        for hand in hand_list[prev_idx:d]:
+            abstraction[hand] = label
+        prev_idx = d
+    label += 1
+    for hand in hand_list[prev_idx:]:
+        abstraction[hand] = label
+
+    # Offset the label so it doesn't clash with the preflop or flop
+    for a in abstraction:
+        abstraction[a] += N_FLOP_BUCKETS + N_PREFLOP_BUCKETS
+
+    pickle.dump(abstraction, open(TURN_SAVE_NAME, 'wb'))
+    return abstraction
+
+
+def prepare_abstraction():
+    """Returns a dict containinig all hand abstractions for Rhode Island Holdem."""
+    preflop = preflop_abstraction()
+    flop = flop_abstraction()
+    turn = turn_abstraction()
+    return {**preflop, **flop, **turn}
+
+
+def print_abstraction():
+    abst = prepare_abstraction()
+    bins = [[] for i in range(N_PREFLOP_BUCKETS + N_FLOP_BUCKETS + N_TURN_BUCKETS)]
+    for a in abst:
+        bins[abst[a]].append(a)
+    print('preflop:')
+    for i, b in enumerate(bins):
+        if i == N_PREFLOP_BUCKETS:
+            print('flop')
+        elif i == N_FLOP_BUCKETS + N_PREFLOP_BUCKETS:
+            print('turn')
+        print('Bin', i, b[:10])
+
 
 
 if __name__ == '__main__':
 
-    import pprint
-    abstraction = compute_abstractions()
-    pprint.pprint(abstraction)
-    pdb.set_trace()
-    # show_equity_distributions()
+    print_abstraction()
+
+    # abstraction = compute_abstractions()
+    # bins = [[] for i in range(N_BUCKETS)]
+    # for hand in abstraction:
+    #     bins[abstraction[hand]].append(hand)
+    # pprint.pprint(bins)
+    # # show_equity_distributions()
