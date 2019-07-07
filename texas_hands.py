108978df3a91cd4e7b76411685d7a6ffbb607bf5
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2019-07-07 12:46:30 -0700

    Add a way to compare hands of the same type.
    If two hands are both, say, two pair, we need to take the rank of their
    cards into account when classifying them. The code for classifying two
    pair hands got complicated and more tests may be needed.

--- texas_hands.py
+++ texas_hands.py
@@ -35,14 +35,26 @@ class TexasHand:
             ValueError if cards contains invalid card strings, the wrong number
             of cards, or duplicate cards.
         """
-        # TODO: Throw the necessary errors.
+        self.check_input(cards)
         self.cards = list(cards)
         self.ranks = sorted([RANKS[card[0]] for card in self.cards])
         self.suits = [card[1] for card in self.cards]
         # How many times each rank appears in the hand (useful for hand classification)
         _, self.counts = np.sort(np.unique(self.ranks, return_counts=True))
         self.type = None
+        # The 5 cards that actually matter if more than 5 are supplied
+        self.playable_cards = None
         self.classify()
+
+    def check_input(self, cards):
+        """Makes sure the cards pass the input specifications."""
+        if not 5 <= len(cards) <= 7:    # Python rocks
+            raise ValueError('Must be between 5 and 7 cards provided')
+        for card in cards:
+            if not isinstance(card, str):
+                raise TypeError('Cards must be strings in the format "5d"')
+            if len(card) != 2 or card[0] not in RANKS or card[1] not in SUITS:
+                raise ValueError('Invalid card string: ' + card)
 
     def classify(self):
         """Identifies which type of poker hand this is."""
@@ -52,8 +64,10 @@ class TexasHand:
                 hand = TexasHand(five_cards)
                 if hand > best_subhand:
                     best_subhand = hand
+                    self.playable_cards = five_cards
             self.type = best_subhand.type
         else:
+            self.playable_cards = self.cards
             if self.is_royal_flush():
                 self.type = ROYAL_FLUSH
             elif self.is_straight_flush():
@@ -118,18 +132,81 @@ class TexasHand:
         return self.is_n_of_a_kind(2)
 
     def __lt__(self, other):
+        if other is None:
+            return False
         if self.type < other.type:
             return True
         elif other.type < self.type:
             return False
         elif self.type == other.type:
-            # TODO: Comparing ranks won't work for two pair when the second pairs
-            # differ but the top pair is the same. Use every rank in turn to compare.
-            return self.compare_ranks(other)
+            return self.compare_ranks(other) == -1
 
     def __eq__(self, other):
-        raise NotImplementedError
+        # TODO: Implement this for real by considering the ranks of the top 5 cards
+        if other is None:
+            return False
+        return self.type == other.type and self.compare_ranks(other) == 0
+
+    def get_two_pair_info(self):
+        """For a two pair hand, returns the ranks of the two pairs and kicker."""
+        if self.type != TWO_PAIR:
+            raise ValueError('Hand must be two pair')
+
+        ranks = [RANKS[card[0]] for card in self.playable_cards]
+        unique, counts = np.unique(ranks, return_counts=True)
+        pairs = []
+        kicker = 0
+        for i in reversed(range(len(counts))):
+            if counts[i] == 2:
+                pairs.append(unique[i])
+            if counts[i] == 1:
+                kicker = unique[i]
+        return pairs, kicker
+
+    def compare_ranks(self, other):
+        """Compares the five playable cards of each hand to see which rank higher.
+
+        This method is useful if both hands are the same type (say, flush) and
+        you want to know which flush is better.
+
+        Inputs:
+            other - TexasHand instance
+
+        Returns:
+            cmp: -1 if self < other, 0 if self == other, and 1 if self > other.
+        """
+        if self.type != other.type:
+            raise ValueError('Hand types must match for the compare_ranks method.')
+        my_ranks = np.sort([RANKS[card[0]] for card in self.playable_cards])[::-1]
+        other_ranks = np.sort([RANKS[card[0]] for card in other.playable_cards])[::-1]
+        # Two pair is a special case that is especially difficult to rank.
+        if self.type == TWO_PAIR:
+            my_pairs, my_kicker = self.get_two_pair_info()
+            other_pairs, other_kicker = other.get_two_pair_info()
+            for my_pair, other_pair in zip(my_pairs, other_pairs):
+                if my_pair < other_pair:
+                    return -1
+                if my_pair > other_pair:
+                    return 1
+
+            if my_kicker < other_kicker:
+                return -1
+            if my_kicker > other_kicker:
+                return 1
+            return 0    # Both pairs and the kicker are all equal.
+
+        for i in range(len(my_ranks)):
+            if my_ranks[i] < other_ranks[i]:
+                return -1
+            elif my_ranks[i] > other_ranks[i]:
+                return 1
+        return 0
+
 
     def __str__(self):
         raise NotImplementedError
+
+
+# TODO: Use lookup tables to speed up hand classification and comparison. Each
+# hand could correspond to a strength integer (like 1453)
 
