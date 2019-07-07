d10c00d19c75838029b94ed41e77f76c31b3a7eb
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2019-07-06 21:24:29 -0700

    Add more code and tests for implementing the TexasHand class.

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ class TestTexasHands(unittest.TestCase):
         trips = TexasHand(('5d', '4c', '6d', '6h', '6c'))
         two_pair = TexasHand(('6d', '5c', '5h', 'Ah', 'Ac'))
         pair = TexasHand(('Ah', '2d', '2s', '3c', '5c'))
-        high_card = TexasHand('Kh', 'Ah', 'Qh', '2h', '3s')
+        high_card = TexasHand(('Kh', 'Ah', 'Qh', '2h', '3s'))
         self.assertEqual(royal_flush.type, ROYAL_FLUSH)
         self.assertEqual(royal_flush.type, ROYAL_FLUSH)
         self.assertEqual(royal_flush.type, ROYAL_FLUSH)
@@ -70,17 +70,99 @@ class TestTexasHands(unittest.TestCase):
         self.assertEqual(royal_flush.type, ROYAL_FLUSH)
 
     def test_six_cards(self):
-        pass
+        royal_flush = TexasHand(('Jd', 'As', 'Js', 'Ks', 'Qs', '10s'))
+        straight_flush = TexasHand(('7d', '2c', '8d', 'Jd', '9d', '10d'))
+        four = TexasHand(('2h', '2c', '3d', '7d', '2d', '2s'))
+        full_house = TexasHand(('As', 'Jd', 'Jc', '2c', 'Ac', 'Ah'))
+        flush = TexasHand(('Jh', '2h', '3h', '7h', 'Ts', '9h'))
+        straight = TexasHand(('Ah', '2s', '3d', '5c', '4c', 'Td'))
+        trips = TexasHand(('Ad', '5d', '4c', '6d', '6h', '6c'))
+        two_pair = TexasHand(('6d', '5c', '5h', 'Ah', 'Ac', '2c'))
+        pair = TexasHand(('Ah', '2d', '2s', '3c', 'Th', '5c'))
+        high_card = TexasHand(('Kh', 'Ah', '9d', 'Qh', '2h', '3s'))
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+
 
     def test_seven_cards(self):
-        pass
+        royal_flush = TexasHand(('Jd', 'As', 'Js', 'Ks', 'Qs', '10s', '2c'))
+        straight_flush = TexasHand(('7d', '2c', '8d', 'Jd', '9d', '3d', '10d'))
+        four = TexasHand(('2h', '2c', '3d', '5c,' '7d', '2d', '2s'))
+        full_house = TexasHand(('As', 'Jd', 'Qs', 'Jc', '2c', 'Ac', 'Ah'))
+        flush = TexasHand(('Jh', '2c', '2h', '3h', '7h', 'Ts', '9h'))
+        straight = TexasHand(('2c', 'Ah', '2s', '3d', '5c', '4c', 'Td'))
+        trips = TexasHand(('2c', 'Ad', '5d', '4c', '6d', '6h', '6c'))
+        two_pair = TexasHand(('6d', '5c', 'Td', '5h', 'Ah', 'Ac', '2c'))
+        pair = TexasHand(('Ah', '2d', '2s', '3c', 'Th', '5c', 'Qh'))
+        high_card = TexasHand(('Kh', 'Ah', '9d', 'Qh', '2h', '6d', '3s'))
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
+        self.assertEqual(royal_flush.type, ROYAL_FLUSH)
 
     # Should raise a value error when incorrect cards are provided.
     def test_errors(self):
-        pass
+        # Invalid card strings
+        self.assertRaises(ValueError, TexasHand(('blah', 'nope', '3h', 'foobar', '7d')))
+        self.assertRaises(ValueError, TexasHand(('Td', 'Th', 'Tc', '10s', '9d', '4h')))
+        # Too many cards
+        self.assertRaises(ValueError, TexasHand(('Td', 'Th', 'Tc', '9c', '3h', 'Ts', '9d', '4h')))
+        # Too few cards
+        self.assertRaises(ValueError, TexasHand(('7c', '7h', '7d', '7s')))
+        # Duplicate cards
+        self.assertRaises(ValueError, TexasHand(('7c', '7c', '7h', '7d', '7s')))
 
     def test_comparisons(self):
-        pass
+        royal_flush = TexasHand(('Jd', 'As', 'Js', 'Ks', 'Qs', '10s', '2c'))
+        straight_flush = TexasHand(('7d', '2c', '8d', 'Jd', '9d', '3d', '10d'))
+        four = TexasHand(('2h', '2c', '3d', '5c,' '7d', '2d', '2s'))
+        full_house = TexasHand(('As', 'Jd', 'Qs', 'Jc', '2c', 'Ac', 'Ah'))
+        same_full_house = TexasHand(('As', 'Js', '2s', 'Jc', '2c', 'Ac', 'Ah'))
+        flush = TexasHand(('Jh', '2c', '2h', '3h', '7h', 'As', '9h'))
+        same_flush = TexasHand(('Jh', '2c', '2h', '3h', '7h', '2s', '9h'))
+        better_flush = TexasHand(('Jh', '2c', 'Ah', '3h', '7h', 'Ts', '9h'))
+        straight = TexasHand(('Ah', '2s', '3d', '5c', '4c'))
+        trips = TexasHand(('5d', '4c', '6d', '6h', '6c'))
+        two_pair = TexasHand(('6d', '5c', '5h', 'Ah', 'Ac'))
+        better_two_pair = TexasHand(('Td', 'Th', 'Ad', 'Ac', '6h'))
+        pair = TexasHand(('Ah', '2d', '2s', '3c', '5c'))
+        ace_pair = TexasHand(('Ac', 'As', '2s', '3d', '6c'))
+        better_kicker = TexasHand(('Ac', 'As', 'Ts', '3d', '6c'))
+        high_card = TexasHand(('Kh', 'Ah', 'Qh', '2h', '3s'))
+        other_high_card = TexasHand(('Ks', 'As', 'Qs', '2h', '3s'))
+
+        # Test random hand type comparisons
+        self.assertTrue(royal_flush > straight_flush)
+        self.assertTrue(royal_flush > trips)
+        self.assertTrue(straight_flush > full_house)
+        self.assertTrue(trips > two_pair)
+        self.assertTrue(high_card < pair)
+        self.assertTrue(straight <= flush)
+
+        # Test rank levels within hands
+        self.assertTrue(better_two_pair > two_pair)
+        self.assertTrue(better_flush > flush)
+        self.assertTrue(better_kicker > ace_pair)
+
+        # Test for ties
+        self.assertEqual(better_two_pair, better_two_pair)
+        self.assertEqual(same_full_house, full_house)
+        self.assertEqual(other_high_card, high_card)
+        self.assertEqual(same_flush, flush)
 
 
 if __name__ == '__main__':
--- texas_hands.py
+++ texas_hands.py
@@ -1,11 +1,13 @@
 """Module for representing and comparing Texas Hold'em hands and abstractions."""
 
 import numpy
+import itertools
 import functools
 import pickle
 import pdb
 
-HIGH_CARD, PAIR, FLUSH, STRAIGHT, THREE_OF_A_KIND, STRAIGHT_FLUSH = range(6)
+(HIGH_CARD, PAIR, TWO_PAIR, THREE_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE,
+ FOUR_OF_A_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH) = range(10)
 
 RANKS = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8, '7': 7,
          '6': 6, '5': 5, '4': 4, '3': 3, '2': 2}
@@ -17,7 +19,7 @@ def get_deck():
     return [rank + suit for suit in SUITS for rank in RANKS]
 
 
-
+# TODO: Do we even really need this card class?
 @functools.total_ordering
 class Card:
     """Class for representing a card using the format '8c', 'Th', etc.
@@ -60,13 +62,61 @@ class TexasHand:
     """Represents a standard 5-card Texas Hold'em hand."""
 
     def __init__(self, cards):
-        raise NotImplementedError
+        """Create a new Texas Holdem hand.
+
+        Inputs:
+            cards - list/tuple of cards in the standard format 'Th' '3s' etc.
+                Must be at least five and no more than seven cards.
+
+        Throws:
+            ValueError if cards contains invalid card strings, the wrong number
+            of cards, or duplicate cards.
+        """
+        self.cards = list(cards)
+        self.type = None
+        self.rank = None
+        self.classify()
 
     def classify(self):
-        raise NotImplementedError
+        """Sets the self.type and self.rank variables to the hand type and rank."""
+        if len(self.cards) > 5:
+            best_subhand = None
+            for five_cards in itertools.combinations(self.cards, 5):
+                hand = TexasHand(five_cards)
+                if hand > best_subhand:
+                    best_subhand = hand
+            self.type = best_subhand.type
+            self.rank = best_subhand.rank
+        else:
+            self.rank = self.max_rank()
+            if self.is_royal_flush():
+                self.type = ROYAL_FLUSH
+            elif self.is_straight_flush():
+                self.type = STRAIGHT_FLUSH
+            elif self.is_four_of_a_kind():
+                self.type = FOUR_OF_A_KIND
+            elif self.is_full_house():
+                self.type = FULL_HOUSE
+            elif self.is_flush():
+                self.type = FLUSH
+            elif self.is_straight():
+                self.type = STRAIGHT
+            elif self.is_three_of_a_kind():
+                self.type = THREE_OF_A_KIND
+            elif self.is_two_pair():
+                self.type = TWO_PAIR
+            else:
+                self.type = HIGH_CARD
 
     def __lt__(self, other):
-        raise NotImplementedError
+        if self.type < other.type:
+            return True
+        elif other.type < self.type:
+            return False
+        elif self.type == other.type:
+            # TODO: Comparing ranks won't work for two pair when the second pairs
+            # differ but the top pair is the same. Use every rank in turn to compare.
+            return self.rank < other.rank
 
     def __eq__(self, other):
         raise NotImplementedError
