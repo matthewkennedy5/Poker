ace54e4887599ab5ea92536d5db4541e67490878
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2019-07-07 01:01:28 -0700

    Add hand classification methods
    These methods pass unit tests showing they correctly identify 5-card
    hands.

--- texas_hands.py
+++ texas_hands.py
@@ -5,6 +5,7 @@ import itertools
 import functools
 import pickle
 import pdb
+import numpy as np
 
 (HIGH_CARD, PAIR, TWO_PAIR, THREE_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE,
  FOUR_OF_A_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH) = range(10)
@@ -34,9 +35,13 @@ class TexasHand:
             ValueError if cards contains invalid card strings, the wrong number
             of cards, or duplicate cards.
         """
+        # TODO: Throw the necessary errors.
         self.cards = list(cards)
+        self.ranks = sorted([RANKS[card[0]] for card in self.cards])
+        self.suits = [card[1] for card in self.cards]
+        # How many times each rank appears in the hand (useful for hand classification)
+        _, self.counts = np.sort(np.unique(self.ranks, return_counts=True))
         self.type = None
-        self.rank = None
         self.classify()
 
     def classify(self):
@@ -65,12 +70,52 @@ class TexasHand:
                 self.type = THREE_OF_A_KIND
             elif self.is_two_pair():
                 self.type = TWO_PAIR
+            elif self.is_pair():
+                self.type = PAIR
             else:
                 self.type = HIGH_CARD
 
     def is_royal_flush(self):
-        if self.is_straight_flush() and self.has_rank('A')
+        # Ace-high straight flush = royal flush
+        return self.is_straight_flush() and RANKS['A'] in self.ranks
 
+    def is_straight_flush(self):
+        return self.is_straight() and self.is_flush()
+
+    def is_n_of_a_kind(self, n):
+        """Returns True if the hand contains n cards of the same rank.
+
+        Will return True for full houses.
+        """
+        return np.max(self.counts) == n
+
+    def is_four_of_a_kind(self):
+        return self.is_n_of_a_kind(4)
+
+    def is_full_house(self):
+        return np.array_equal(self.counts, np.array([2, 3]))
+
+    def is_flush(self):
+        suit = self.cards[0][1]
+        for card in self.cards:
+            if card[1] != suit:
+                return False
+        return True
+
+    def is_straight(self):
+        # True if the ranks are the same as the range (a straight)
+        return (self.ranks == list(range(self.ranks[0], self.ranks[-1] + 1))
+                or self.ranks == [2, 3, 4, 5, 14])    # Ace low straight
+
+    def is_three_of_a_kind(self):
+        return self.is_n_of_a_kind(3)
+
+    def is_two_pair(self):
+        # A two pair hand has one unique card and two duplicate-rank cards
+        return np.array_equal(self.counts, np.array([1, 2, 2]))
+
+    def is_pair(self):
+        return self.is_n_of_a_kind(2)
 
     def __lt__(self, other):
         if self.type < other.type:
