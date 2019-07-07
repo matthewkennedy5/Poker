723b58df49a36d95415664221e2daebc9facef58
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2019-07-06 20:17:55 -0700

    Change test code to use the unittest functionality.
    Because the tests were being performed with assert, it wasn't even
    using the unittest testing functionality. unittest wasn't running the
    tests because the methods weren't prefixed with 'test'.

--- test.py
+++ test.py
@@ -1,54 +1,55 @@
-from poker_utils import *
 import numpy as np
 import pdb
 import unittest
 import rhode_utils
 import rhode_trainer
+from texas_hands import *
+
 
 FOLD, CHECK, CALL, BET, RAISE = range(5)
 
-if __name__ == '__main__':
-    TestRaiseLimit.main()
 
 class TestRaiseLimit(unittest.TestCase):
-    def test(self, bet_history):
+
+    def run_test(self, bet_history):
         deck = rhode_utils.get_deck()
         trainer = rhode_trainer.CFRPTrainer()
         info = rhode_trainer.InfoSet(deck, bet_history)
         return info.legal_actions()
 
-    def no_raises(self):
+    def test_no_raises(self):
         bet_history = ['check', 'bet', 'call', 'check', 'check']
-        actions = self.test(bet_history)
-        assert actions == [CHECK, BET]
+        actions = self.run_test(bet_history)
+        self.assertEqual(actions, [CHECK, BET])
 
-    def empty(self):
+    def test_empty(self):
         bet_history = []
-        actions = self.test(bet_history)
-        assert actions == [CHECK, BET]
+        actions = self.run_test(bet_history)
+        self.assertTrue(actions == [CHECK, BET])
 
-    def many_raises(self):
+    def test_many_raises(self):
         bet_history = ['bet', 'raise', 'raise', 'raise', 'call',
             'bet', 'raise', 'raise', 'call', 'bet', 'raise']
-        actions = self.test(bet_history)
-        assert actions == [FOLD, CALL, RAISE]
+        actions = self.run_test(bet_history)
+        self.assertTrue(actions == [FOLD, CALL, RAISE])
 
-    def cant_raise(self):
+    def test_cant_raise(self):
         bet_history = ['bet', 'raise', 'raise', 'raise', 'call',
             'bet', 'raise', 'raise', 'raise']
-        actions = self.test(bet_history)
-        assert actions == [FOLD, CALL]
+        actions = self.run_test(bet_history)
+        self.assertTrue(actions == [FOLD, CALL])
 
-    def last_raise(self):
+    def test_last_raise(self):
         bet_history = ['check', 'bet', 'raise', 'raise']
-        actions = self.test(bet_history)
-        assert actions == [FOLD, CALL, RAISE]
+        actions = self.run_test(bet_history)
+        self.assertTrue(actions == [FOLD, CALL, RAISE])
 
-    def main()
-        tester = TestRaiseLimit()
-        tester.no_raises()
-        tester.empty()
-        tester.many_raises()
-        tester.cant_raise()
-        tester.last_raise()
-        print('Raise Limit Works Properly')
+
+class TestTexasHands(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
