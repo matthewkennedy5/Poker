08fa5184d37837de1ef403f7485d6f53552b9325
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2020-02-24 20:45:25 -0600

    More progress on trainer.rs
    Implemented about 50% of iterate() with stubs for the dependent
    functions. Had to change types around in this commit, specifically,
    weights has to be an array and utilities have to be doubles, not ints.

--- rust/src/trainer.rs
+++ rust/src/trainer.rs
@@ -6,7 +6,17 @@ use rand::prelude::SliceRandom;
 use crate::card_utils;
 use crate::card_utils::Card;
 
+// TODO: Use a parameter file
 const BLUEPRINT_STRATEGY_PATH: &str = "blueprint.json";
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize)]
+enum ActionType {
+    FOLD,
+    CALL,
+    BET
+}
+
+// TODO: Chance sample opponent actions or all possible opponent actions?
 
 // Writes out the approximate Nash equilibrium strategy to a JSON
 pub fn train(iters: i32) {
@@ -17,9 +27,9 @@ pub fn train(iters: i32) {
     let bar = card_utils::pbar(iters as u64);
     for i in 0..iters {
         deck.shuffle(&mut rng);
-        iterate(0, &deck, ActionHistory::new(), (0.0, 0.0));
+        iterate(0, &deck, ActionHistory::new(), [0.0, 0.0], &mut nodes);
         deck.shuffle(&mut rng);
-        iterate(1, &deck, ActionHistory::new(), (0.0, 0.0));
+        iterate(1, &deck, ActionHistory::new(), [0.0, 0.0], &mut nodes);
         bar.inc(1);
     }
     bar.finish();
@@ -29,26 +39,55 @@ pub fn train(iters: i32) {
     file.write_all(json.as_bytes());
 }
 
-fn iterate(player: i32, deck: &[Card], history: ActionHistory, weights: (f64, f64)) -> i32 {
+fn iterate(player: usize, deck: &[Card], history: ActionHistory,
+           weights: [f64; 2], nodes: &mut HashMap<InfoSet, Node>) -> f64 {
     if history.hand_over() {
         return terminal_utility(&deck, history, player);
     }
-    return 0;
-    // TODO
+
+    let mut infoset = InfoSet::from(&deck, &history);
+    if !nodes.contains_key(&infoset) {
+        nodes.insert(infoset.clone(), Node::new(&infoset));
+    }
+    let mut node = nodes.get(&infoset).unwrap();
+
+    let opponent = 1 - player;
+    if history.whose_turn() == opponent {
+        // Sample a move from the opponent
+        history.add(opponent_action(node));
+        if history.hand_over() {
+            return terminal_utility(&deck, history, player);
+        }
+        infoset = InfoSet::from(&deck, &history);
+        if !nodes.contains_key(&infoset) {
+            nodes.insert(infoset.clone(), Node::new(&infoset));
+        }
+        node = nodes.get(&infoset).unwrap();
+    }
+
+    let [p0, p1] = weights;
+    let strategy = node.current_strategy(weights[player]);
+    let mut utility: HashMap<Action, f64> = HashMap::new();
+    let mut node_utility = 0.0;
+    node_utility
 }
 
-fn terminal_utility(deck: &[Card], history: ActionHistory, player: i32) -> i32 {
-    // TODO
-    0
+fn opponent_action(node: &Node) -> Action {
+    Action { action: ActionType::FOLD, amount: 0}
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+fn terminal_utility(deck: &[Card], history: ActionHistory, player: usize) -> f64 {
+    // TODO
+    0.0
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize)]
 struct Action {
-    action: String,
+    action: ActionType,
     amount: i32
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize)]
 struct ActionHistory {
     preflop: Vec<Action>,
     flop: Vec<Action>,
@@ -71,12 +110,30 @@ impl ActionHistory {
         // TODO
         false
     }
+
+    pub fn whose_turn(&self) -> usize {
+        // TODO
+        return 0;
+    }
+
+    pub fn add(&self, action: Action) {
+        // TODO
+    }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize)]
 struct InfoSet {
     history: ActionHistory,
     card_bucket: i32
+}
+
+impl InfoSet {
+
+    pub fn from(deck: &[Card], history: &ActionHistory) -> InfoSet {
+        // TODO
+        InfoSet {history: ActionHistory::new(), card_bucket: 0}
+    }
+
 }
 
 #[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
@@ -84,4 +141,19 @@ struct Node {
     infoset: InfoSet,
     regrets: HashMap<Action, f64>,
     t: i32
+}
+
+impl Node {
+
+    pub fn new(infoset: &InfoSet) -> Node {
+        Node {
+            infoset: infoset.clone(),
+            regrets: HashMap::new(),
+            t: 0
+        }
+    }
+
+    pub fn current_strategy(&self, prob: f64) -> HashMap<Action, f64> {
+        HashMap::new()
+    }
 }
