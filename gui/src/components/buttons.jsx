5c26d161690f8a7306fc02808a7c3f7d7bb6a5cd
Matthew Kennedy <mtthwknnd4@gmail.com>
2023-01-25 11:46:43 +0100

    starting tailwind

--- gui/src/components/buttons.jsx
+++ gui/src/components/buttons.jsx
@@ -4,43 +4,81 @@ const BUTTON_STYLE = "btn btn-custom m-1"
 
 class Buttons extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            raiseUIVisible: false,
+        }
+        this.showRaiseUI = this.showRaiseUI.bind(this);
+        this.hideRaiseUI = this.hideRaiseUI.bind(this);
+    }
+
+    showRaiseUI() {
+        this.setState({raiseUIVisible: true});
+    }
+
+    hideRaiseUI() {
+        this.setState({raiseUIVisible: false});
+    }
+
     render() {
         const enabled = this.props.enabledButtons;
+
+        let call_text = "CALL";
+        if (this.props.callAmount > 0) {
+            call_text += " $" + this.props.callAmount;
+        }
+
         if (enabled["nextHand"]) {
             return (
                 <div className="buttons">
                     <button onClick={this.props.nextHand} className={BUTTON_STYLE}>
-                        Next Hand
+                        NEXT HAND
                     </button>
                 </div>
-            )
-        } else {
+            );
+        } else if (this.state.raiseUIVisible) {
+            // Display slider and options for betting
             return (
                 <div className="buttons">
 
-                    {enabled["fold"] ? 
-                        (<button onClick={this.props.fold} className={BUTTON_STYLE} disabled={!enabled["fold"]}>
-                            Fold
-                        </button>)
-                        : (<button onClick={this.props.nextHand} className={BUTTON_STYLE} disabled={!enabled["nextHand"]}>
-                            Next Hand
-                        </button>)}
+                    <div className="raise-ui">
+
+                        <input type="range"
+                            className="range-slider m-1"
+                            min={this.props.minBetAmount}
+                            max={this.props.allInAmount}
+                            onChange={this.props.updateCustomBet}/>
+
+                    </div>
+
+
+                    <button onClick={this.hideRaiseUI} className={BUTTON_STYLE + " back"}>BACK</button>
+
+                </div>
+            );
+        } else {
+            // Normal row of buttons: Call Raise Fold
+            return (
+                <div className="buttons">
 
                     {enabled["check"] ?
                         (<button onClick={this.props.check} className={BUTTON_STYLE} disabled={!enabled["check"]}>
-                            Check
+                            CHECK
                         </button>)
                         : (<button onClick={this.props.call} className={BUTTON_STYLE} disabled={!enabled["call"]}>
-                            Call ${this.props.callAmount}
+                            {call_text}
                         </button>)}
 
-                    <button onClick={this.props.betCustom} className={BUTTON_STYLE} disabled={!enabled["betCustom"]}>
-                        Raise
+                    <button onClick={this.showRaiseUI} className={BUTTON_STYLE} disabled={!enabled["betCustom"]}>
+                        RAISE
                     </button>
-                
-                <h1 className="dollar">$</h1>
 
-                <input type="text" onChange={this.props.updateCustomBet} className="m-1" size="5"></input>
+                    <button onClick={this.props.fold} className={BUTTON_STYLE + " fold"} disabled={!enabled["fold"]}>
+                        FOLD
+                    </button>
+
+                {/* <input type="text" onChange={this.props.updateCustomBet} className="m-1" size="5"></input> */}
 
                 </div>
             );
