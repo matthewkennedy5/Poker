dca5b1cbda159372bcfc3b44dd5da8e00e7efaa4
Matthew Kennedy <mtthwknnd4@gmail.com>
2023-02-14 23:24:09 +0100

    improved frontend (still needs work)

--- gui/src/components/table.jsx
+++ gui/src/components/table.jsx
@@ -5,26 +5,30 @@ class Table extends Component {
     render() {
         return (
             <div className="poker-table">
-                <div className="cards">
-                    <div className="cpu-cards">
-                        <img className="single-card" src={"/cards/" + this.props.cpuCards[0] + ".svg"} alt=""></img>
-                        <img className="single-card" src={"/cards/" + this.props.cpuCards[1] + ".svg"} alt=""></img>
-                    </div>
-                    <div className="board-cards">
-                        <img className="single-card" src={"/cards/" + this.props.board[0] + ".svg"} alt=""></img>
-                        <img className="single-card" src={"/cards/" + this.props.board[1] + ".svg"} alt=""></img>
-                        <img className="single-card" src={"/cards/" + this.props.board[2] + ".svg"} alt=""></img>
-                        <img className="single-card" src={"/cards/" + this.props.board[3] + ".svg"} alt=""></img>
-                        <img className="single-card" src={"/cards/" + this.props.board[4] + ".svg"} alt=""></img>
-                    </div>
-                    <div className="human-cards">
-                        <img className="single-card" src={"/cards/" + this.props.humanCards[0] + ".svg"} alt=""></img>
-                        <img className="single-card" src={"/cards/" + this.props.humanCards[1] + ".svg"} alt=""></img>
-                    </div>
+                <div className="cpu-stack">
+                    <label>stack: ${this.props.stacks["cpu"]}</label>
                 </div>
-                <label className="pot">pot: ${this.props.pot}</label>
-                <label className="cpu-stack">stack: ${this.props.stacks["cpu"]}</label>
-                <label className="human-stack">stack: ${this.props.stacks["human"]}</label>
+                <div className="cpu-cards">
+                    <img className="single-card" src={"/cards/" + this.props.cpuCards[0] + ".svg"} alt=""></img>
+                    <img className="single-card" src={"/cards/" + this.props.cpuCards[1] + ".svg"} alt=""></img>
+                </div>
+                <div className="board-cards">
+                    <img className="single-card" src={"/cards/" + this.props.board[0] + ".svg"} alt=""></img>
+                    <img className="single-card" src={"/cards/" + this.props.board[1] + ".svg"} alt=""></img>
+                    <img className="single-card" src={"/cards/" + this.props.board[2] + ".svg"} alt=""></img>
+                    <img className="single-card" src={"/cards/" + this.props.board[3] + ".svg"} alt=""></img>
+                    <img className="single-card" src={"/cards/" + this.props.board[4] + ".svg"} alt=""></img>
+                </div>
+                <div className="pot">
+                    <label>pot: ${this.props.pot}</label>
+                </div>
+                <div className="human-cards">
+                    <img className="single-card" src={"/cards/" + this.props.humanCards[0] + ".svg"} alt=""></img>
+                    <img className="single-card" src={"/cards/" + this.props.humanCards[1] + ".svg"} alt=""></img>
+                </div>
+                <div className="human-stack">
+                    <label>stack: ${this.props.stacks["human"]}</label>
+                </div>
                 <label className="cpu-action">{this.props.cpuActionText}</label>
                 <label className="human-action">{this.props.humanActionText}</label>
             </div>
