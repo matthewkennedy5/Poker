68656c4d3a2e560fc24b4810cc81e6abf404b60b
Matthew Kennedy <mtthwknnd4@gmail.com>
2023-03-31 16:34:36 -0300

    remove TODO

--- rust/src/bin/preflop_chart.rs
+++ rust/src/bin/preflop_chart.rs
@@ -1,7 +1,6 @@
 use optimus::*;
 
 fn main() {
-    // TODO: Use bot instead of nodes here
     let nodes = load_nodes(&CONFIG.nodes_path);
     write_preflop_strategy(&nodes, &CONFIG.preflop_strategy_path);
 }
