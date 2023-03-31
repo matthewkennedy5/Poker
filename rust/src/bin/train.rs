a7b47c5cf53d351039573980515e107e40172ee0
Matthew Kennedy <mtthwknnd4@gmail.com>
2023-03-31 14:51:24 -0300

    delete comments

--- rust/src/bin/train.rs
+++ rust/src/bin/train.rs
@@ -2,7 +2,4 @@ use optimus::*;
 
 fn main() {
     train(CONFIG.train_iters);
-    // println!("Starting...");
-    // let iters = train_until_convergence();
-    // println!("Took {} iters to converge.", iters);
 }
